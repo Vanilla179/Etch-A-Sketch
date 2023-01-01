5c5dd95c1f821fb96cb48528f2b309dfe7bf19cf
Carlos Rodriguez <Vanilla6644@gmail.com>
2023-01-01 05:46:38 -0700

    done

--- javascript.js
+++ javascript.js
@@ -2,8 +2,13 @@ const container = document.querySelector(".container");
 const wrapper = document.querySelector(".wrapper");
 
 function makeDiv(params) {
+  container.innerHTML = ""; // Clear the current grid
   let Number;
   Number = prompt("Please enter a number");
+
+  if (Number > 100) {
+    return;
+  }
 
   for (let i = 0; i < Number; i++) {
     const div1 = document.createElement("div");
@@ -16,15 +21,9 @@ function makeDiv(params) {
       div2.classList.add("column");
       div2.setAttribute("style", " background: #ededed; ");
       div1.append(div2);
-      div2.onmouseover = function () {
-        div2.setAttribute("style", " background: #24264D; ");
-      };
     }
   }
 }
-
-const btn0 = document.querySelector(".clear");
-btn0.addEventListener("click", function () {});
 
 const btn1 = document.querySelector(".size");
 btn1.addEventListener("click", function () {
@@ -33,22 +32,24 @@ btn1.addEventListener("click", function () {
 
 const btn2 = document.querySelector(".pen");
 btn2.addEventListener("click", function () {
-  pen();
+  const div2Elements = document.querySelectorAll(".column");
+  div2Elements.forEach((div2) => pen(div2));
 });
 
 const btn3 = document.querySelector(".eraser");
 btn3.addEventListener("click", function () {
-  eraser();
+  const div3Elements = document.querySelectorAll(".column");
+  div3Elements.forEach((div2) => eraser(div2));
 });
 
-function pen(params) {
+function pen(div2) {
   div2.onmouseover = function () {
-    div2.setAttribute("style", " background: red; ");
+    div2.setAttribute("style", " background:  #24264D;  ");
   };
 }
 
-function eraser(params) {
+function eraser(div2) {
   div2.onmouseover = function () {
-    div2.setAttribute("style", " background: pink; ");
+    div2.setAttribute("style", " background: #ededed; ");
   };
 }
