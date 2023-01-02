5d7756f2fa675ae8b6732559972ea82b5349c6a3
Carlos Rodriguez <Vanilla6644@gmail.com>
2023-01-02 06:06:50 -0700

    organize Code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
+// Variables for elements in the DOM
 const container = document.querySelector(".container");
 const wrapper = document.querySelector(".wrapper");
+const btn1 = document.querySelector(".size");
+const btn2 = document.querySelector(".pen");
+const btn3 = document.querySelector(".eraser");
 
 // Function to create a grid of divs based on user input
-function makeDiv(params) {
+function makeDiv() {
   // Clear the current grid
   container.innerHTML = "";
 
@@ -29,28 +33,6 @@ function makeDiv(params) {
   }
 }
 
-// Button to create a new grid
-const btn1 = document.querySelector(".size");
-btn1.addEventListener("click", function () {
-  makeDiv();
-});
-
-// Button to apply color to divs
-const btn2 = document.querySelector(".pen");
-btn2.addEventListener("click", function () {
-  // Select all columns and apply color on mouseover
-  const div2Elements = document.querySelectorAll(".column");
-  div2Elements.forEach((div2) => applyColor(div2));
-});
-
-// Button to erase color from divs
-const btn3 = document.querySelector(".eraser");
-btn3.addEventListener("click", function () {
-  // Select all columns and erase color on mouseover
-  const div3Elements = document.querySelectorAll(".column");
-  div3Elements.forEach((div2) => eraseColor(div2));
-});
-
 // Function to apply color to div
 function applyColor(div2) {
   div2.onmouseover = function () {
@@ -64,3 +46,22 @@ function eraseColor(div2) {
     div2.setAttribute("style", " background: #ededed; ");
   };
 }
+
+// Button to create a new grid
+btn1.addEventListener("click", function () {
+  makeDiv();
+});
+
+// Button to apply color to divs
+btn2.addEventListener("click", function () {
+  // Select all columns and apply color on mouseover
+  const div2Elements = document.querySelectorAll(".column");
+  div2Elements.forEach((div2) => applyColor(div2));
+});
+
+// Button to erase color from divs
+btn3.addEventListener("click", function () {
+  // Select all columns and erase color on mouseover
+  const div3Elements = document.querySelectorAll(".column");
+  div3Elements.forEach((div2) => eraseColor(div2));
+});
