b28ff56ff8c285fbafc483a204484644b3425e68
Carlos Rodriguez <Vanilla6644@gmail.com>
2023-01-02 05:56:47 -0700

    add comments

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 const wrapper = document.querySelector(".wrapper");
 
+// Function to create a grid of divs based on user input
 function makeDiv(params) {
-  container.innerHTML = ""; // Clear the current grid
+  // Clear the current grid
+  container.innerHTML = "";
+
   let Number;
   Number = prompt("Please enter a number");
 
@@ -10,6 +13,7 @@ function makeDiv(params) {
     return;
   }
 
+  // Create rows and columns of divs
   for (let i = 0; i < Number; i++) {
     const div1 = document.createElement("div");
     div1.classList.add("row");
@@ -25,30 +29,37 @@ function makeDiv(params) {
   }
 }
 
+// Button to create a new grid
 const btn1 = document.querySelector(".size");
 btn1.addEventListener("click", function () {
   makeDiv();
 });
 
+// Button to apply color to divs
 const btn2 = document.querySelector(".pen");
 btn2.addEventListener("click", function () {
+  // Select all columns and apply color on mouseover
   const div2Elements = document.querySelectorAll(".column");
-  div2Elements.forEach((div2) => pen(div2));
+  div2Elements.forEach((div2) => applyColor(div2));
 });
 
+// Button to erase color from divs
 const btn3 = document.querySelector(".eraser");
 btn3.addEventListener("click", function () {
+  // Select all columns and erase color on mouseover
   const div3Elements = document.querySelectorAll(".column");
-  div3Elements.forEach((div2) => eraser(div2));
+  div3Elements.forEach((div2) => eraseColor(div2));
 });
 
-function pen(div2) {
+// Function to apply color to div
+function applyColor(div2) {
   div2.onmouseover = function () {
     div2.setAttribute("style", " background:  #24264D;  ");
   };
 }
 
-function eraser(div2) {
+// Function to erase color from div
+function eraseColor(div2) {
   div2.onmouseover = function () {
     div2.setAttribute("style", " background: #ededed; ");
   };
